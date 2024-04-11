9218708bd2f8c2f6db12f521ee725f4713c03479
lakshmanarao <lakshmanarao.pokuri121@gmail.com>
2024-04-10 19:40:20 -0500

    edit ssh keys

--- compute.tf
+++ compute.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance" "demo-instance" {
     }
   }
   metadata = {
-    ssh-keys = "lakshmana:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUwlv5ori5D5uocQB47IF1a4TBTwaj/0Fx7QfGlQFM4qpTbv0BiGZI5KUhYE20G1/pkaeyzgtS5CD5h2qwwel8hlY5dtOf0kTrMqH17yBQQ7bRyL9DIDuPzTRroGgXZlPb/CAKhFFgya+2pNZBRvChi/471ndNCyA5ZOY9e/fHhcqg6jqxYY8Y0bnTREU5Zu0icsoVjRBkpFQQvnzkq00cE84zqBlXkVKqj6snTfULveuomvAGSTIxGNZaSrrBXyHixHLJT0HnGCEsOp2xJZZZlKl94THp7GhMSQgLW0di1ppcY87X8/k2cd4I30FS3cYH0zkB4A0HYsF82Bc9AVx01alTIcxVGCDSsafQM5FrhjOUP4DoCI9cigODXbqQp8fYe++w8xaJyCgGYL35Kzh1yMC5tQOPdLU43udR8z8F+LInL6QQy7KcVKjOkWym+1NZQSJhe5SmCR9L2r0de65ug0bjVVv2PITFG94auE4yxPItGd0xS/vNXIfXgLkdhJrfWzAIvzR/8L4PjNKofyalvCMLUWElbfDda3E0XqFuTSa5gl2Rhxf9jSq6ufOt/DX9J7Z3M8jatDaqXI7N7XQjQDYTr1vHeGcOG2/1MiTU6z0svzy5C8seTyV4t9EXDgMoiS7fBNT8vIj+2URtisatvB7p3wiMg9X56dHjBoXOhw== lakshmanarao"
+    ssh-keys = ""
   }
 }
 # external ip for masternode
@@ -54,6 +54,6 @@ resource "google_compute_instance" "master-instance" {
     }
   }
   metadata = {
-    ssh-keys = "lakshmana:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUwlv5ori5D5uocQB47IF1a4TBTwaj/0Fx7QfGlQFM4qpTbv0BiGZI5KUhYE20G1/pkaeyzgtS5CD5h2qwwel8hlY5dtOf0kTrMqH17yBQQ7bRyL9DIDuPzTRroGgXZlPb/CAKhFFgya+2pNZBRvChi/471ndNCyA5ZOY9e/fHhcqg6jqxYY8Y0bnTREU5Zu0icsoVjRBkpFQQvnzkq00cE84zqBlXkVKqj6snTfULveuomvAGSTIxGNZaSrrBXyHixHLJT0HnGCEsOp2xJZZZlKl94THp7GhMSQgLW0di1ppcY87X8/k2cd4I30FS3cYH0zkB4A0HYsF82Bc9AVx01alTIcxVGCDSsafQM5FrhjOUP4DoCI9cigODXbqQp8fYe++w8xaJyCgGYL35Kzh1yMC5tQOPdLU43udR8z8F+LInL6QQy7KcVKjOkWym+1NZQSJhe5SmCR9L2r0de65ug0bjVVv2PITFG94auE4yxPItGd0xS/vNXIfXgLkdhJrfWzAIvzR/8L4PjNKofyalvCMLUWElbfDda3E0XqFuTSa5gl2Rhxf9jSq6ufOt/DX9J7Z3M8jatDaqXI7N7XQjQDYTr1vHeGcOG2/1MiTU6z0svzy5C8seTyV4t9EXDgMoiS7fBNT8vIj+2URtisatvB7p3wiMg9X56dHjBoXOhw== lakshmanarao"
+    ssh-keys = ""
   }
 }
